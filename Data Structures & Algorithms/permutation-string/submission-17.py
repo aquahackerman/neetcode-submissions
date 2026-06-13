397fbdb6351946e54c21a86725066301b0ee30f1
aquahackerman <140491945+aquahackerman@users.noreply.github.com>
2026-06-13 17:25:38 +0530

    Add: permutation-string - submission-17

---
+++ Data Structures & Algorithms/permutation-string/submission-17.py
@@ -0,0 +1,47 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        d = {}
+        for letter in s1:
+            if letter not in d:
+                d[letter] = 1
+            else:
+                d[letter] += 1
+        
+        l,r = 0,0 
+        temp = d.copy()
+        while l < len(s2):
+            letter = s2[l]
+            if letter in d:
+                d = temp.copy()
+                count = 0
+                r = l
+                while r - l + 1 <= len(s1) and r < len(s2):
+                    print(l,r)
+                    letter = s2[r]
+                    if letter not in d:
+                        break
+                    d[letter] -= 1
+                    if d[letter] == 0:
+                        count += 1
+                        if count == len(d):
+                            return True
+                    elif d[letter] == -1:
+                        while s2[l] != letter:
+                            d[s2[l]] += 1
+                            l += 1
+                            count -= 1
+                        l += 1
+                        d[letter] = 0
+                    r += 1
+            while (l != r):
+                d[s2[l]] += 1
+                l += 1
+            l += 1
+            r += 1
+                    
+
+        return False
+
+                
+
+        
